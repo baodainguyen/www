5552e41d6df32c8a919fff929c1fb6d65d4ca2c3
Nguyen Bao Dai <dainb@Dais-McMini.local>
2019-06-22 22:25:32 +0700

    new archi

--- binddata.js
+++ binddata.js
@@ -2,11 +2,11 @@ var navigation = {
     "avatar" : "http://cdn.collider.com/uploads/imagegallery/iron_man_movie/iron_man_movie_image.jpg",
     "navbar" :  
         [
-            { "url" : "https://raw.githubusercontent.com/baodainguyen/www/master/test/test-page-content.html"  , "icon": "fa-home", "name" : "Home"},
-            { "url" : "https://raw.githubusercontent.com/baodainguyen/www/master/technical.html"  , "icon": "fa-envelope", "name" : "Projects"},
-            { "url" : "load-gallery"  , "icon": "fa-image", "name" : "Gallery"},
-            { "url" : "load-about"  , "icon": "fa-user", "name" : "Personal"},
-            { "url" : "load-more-contact-knowledges"  , "icon": "fa-ellipsis-h", "name" : "More"}
+            { "url" : "https://raw.githubusercontent.com/baodainguyen/www/master/test/test-page-content.html"  , "icon": "fa-home", "name" : "Home", "type": "loadpage"},
+            { "url" : "https://raw.githubusercontent.com/baodainguyen/www/master/technical.html"  , "icon": "fa-code", "name" : "Projects", "type": "loadpage"},
+            { "url" : "load-gallery"  , "icon": "fa-image", "name" : "Gallery", "type": "loadpage"},
+            { "url" : "load-about"  , "icon": "fa-gg", "name" : "Technic", "type": "loadpage"},
+            { "url" : "http://www.load-more-contact-knowledges"  , "icon": "fa-ellipsis-h", "name" : "More", "type": "newpage"}
         ]
 };
 $(document).ready(function(){
@@ -28,8 +28,16 @@ function controlNav(){
     loopNav(ipadNav, iphoneNav);
     loopNav(iphoneNav, ipadNav);
     function loadAndRemoveAllSelector(selector){
+        var loadType = $(selector).attr('loadtype');
         var url = $(selector).attr('loadhref');
-        $('#main-page-content').load(url);
+        switch(loadType){
+            case 'loadpage' :
+                $('#main-page-content').load(url);
+                break;
+            case 'newpage' :
+                window.open(url);
+                break;
+        };
         iphoneNav.each(function() { $(this).removeClass('w3-black'); });
         ipadNav.each(function() { $(this).removeClass('w3-black'); });
     };
