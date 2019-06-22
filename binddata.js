c8f424f063825c2ab6a08954ac21902469565955
Nguyen Bao Dai <dainb@Dais-McMini.local>
2019-06-22 21:46:11 +0700

    udpate site

--- binddata.js
+++ binddata.js
@@ -3,10 +3,10 @@ var navigation = {
     "navbar" :  
         [
             { "url" : "https://raw.githubusercontent.com/baodainguyen/www/master/test/test-page-content.html"  , "icon": "fa-home", "name" : "Home"},
-            { "url" : "https://raw.githubusercontent.com/baodainguyen/www/master/technical.html"  , "icon": "fa-user", "name" : "About"},
-            { "url" : "#photos"  , "icon": "fa-image", "name" : "Photos"},
-            { "url" : "#contact"  , "icon": "fa-envelope", "name" : "Contact"},
-            { "url" : "#"  , "icon": "fa-ellipsis-h", "name" : "More"}
+            { "url" : "https://raw.githubusercontent.com/baodainguyen/www/master/technical.html"  , "icon": "fa-envelope", "name" : "Projects"},
+            { "url" : "load-gallery"  , "icon": "fa-image", "name" : "Gallery"},
+            { "url" : "load-about"  , "icon": "fa-user", "name" : "Personal"},
+            { "url" : "load-more-contact-knowledges"  , "icon": "fa-ellipsis-h", "name" : "More"}
         ]
 };
 $(document).ready(function(){
@@ -25,24 +25,22 @@ function bindView(){
 function controlNav(){
     var ipadNav = $('#navigator-bar > nav > div').children();
     var iphoneNav = $('#navigator-bar div.w3-bar').children();
-    ipadNav.each(function(index) {
-        $(this).click(function(){
-            loadAndRemoveAllSelector(this);
-            $(this).addClass('w3-black');
-            $(iphoneNav[index]).addClass('w3-black');
-        });
-    });
-    iphoneNav.each(function(index) {
-        $(this).click(function(){
-            loadAndRemoveAllSelector(this);
-            $(this).addClass('w3-black');
-            $(ipadNav[index]).addClass('w3-black');
-        });
-    });
+    loopNav(ipadNav, iphoneNav);
+    loopNav(iphoneNav, ipadNav);
     function loadAndRemoveAllSelector(selector){
         var url = $(selector).attr('loadhref');
         $('#main-page-content').load(url);
         iphoneNav.each(function() { $(this).removeClass('w3-black'); });
         ipadNav.each(function() { $(this).removeClass('w3-black'); });
+    };
+    function loopNav(arrayNav1, arrayNav2){
+        arrayNav1.each(function(index) {
+        $(this).click(function(){
+            if($(this).hasClass('w3-black')) return;
+            loadAndRemoveAllSelector(this);
+            $(this).addClass('w3-black');
+            $(arrayNav2[index]).addClass('w3-black');
+        });
+    });
     };
 };
