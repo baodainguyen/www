a3a6081cb0eeb88d3b63968b0e0fb1b6a1e1ac2f
Nguyen Bao Dai <dainb@Dais-McMini.local>
2019-07-15 22:32:06 +0700

    update nav style

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Change style of navbar on scroll
-window.onscroll = function() {myFunction()};
-function myFunction() {
-    var navbar = document.getElementById("navbar-desktop");
+window.onscroll = function() {
+  var navbar = document.getElementById("navbar-desktop");
     if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+        navbar.className = "w3-bar" + " w3-card" + " w3-top" + " w3-white";
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", " w3-text-white");
+        navbar.className = navbar.className.replace(" w3-card w3-top w3-white", " w3-text-white");
     }
 };
 function sendMail(emailAddress, mailContent){
