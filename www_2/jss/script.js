8d279c1986b5fa8756ec9de016603884e4fa4d4c
DaiNB-PC-056 <nguyen.ba.dai@kloon.vn>
2020-03-03 17:32:27 +0700

    update flex display

--- www_2/jss/script.js
+++ www_2/jss/script.js
@@ -1,42 +1,4 @@
-var data = [
-    {
-        id: 0,
-        imgTitle: "https://www.w3schools.com/w3images/avatar5.png",
-        title: "Pet project 1",
-        date: "2020-01-05T17:00:00.000Z",
-        content: "",
-        images: "",
-        view: 26,
-        like: 7,
-        viewMore: "https://baodainguyen.github.io/Gg-Coding-Int/",
-        html: "<p>Here is small pet project.↵I am so excited to introduce you a piece of algorithm how to find available time from 2 people, they have meeting and working time. </p>↵<ol>↵<li><p>Person 1 has meeting time [['9:00', '10:30'], ['12:00', '13:00'], ['16:00', '18:00']] </p><p>and working time ['9:00', '20:00'].</p></li>↵<li><p>Person 2 has meeting time [['10:00', '11:30'], ['12:30', '14:30'], ['14:30', '15:00'], ['16:00', '17:00']] </p><p>and working time ['10:00', '18:30']</p></li>↵</ol>↵<p>With time range is 30 minutes: Let find available time to setup metting time for them together (about 30 minutes from time range).↵</p>↵<p>More info please visit github page in View more.</p>"
-    },
-    {
-        id: 1,
-        imgTitle: "https://www.w3schools.com/w3images/avatar2.png",
-        title: "Project Demo 3",
-        date: "2019-11-05T17:00:00.000Z",
-        content: "This is pet project - Demo 3, html page in view more button (include mobile frendly). Thank you for watching!",
-        images: "https://live.staticflickr.com/65535/49022127126_1c21669229_b.jpg",
-        view: 37,
-        like: 7,
-        viewMore: "https://baodainguyen.github.io/demo03/",
-        html: "",
-    },
-    {
-        id: 2,
-        imgTitle: "https://www.w3schools.com/w3images/avatar5.png",
-        title: "Project Demo 2",
-        date: "2019-10-17T17:00:00.000Z",
-        content: "This is pet project - Demo2, html page in view more button. Thank you for watching!",
-        images: "https://live.staticflickr.com/65535/49339746757_1730d47f7d_k.jpg",
-        view: 40,
-        like: 8,
-        viewMore: "https://baodainguyen.github.io/demo02/",
-        html: ""
-    }
-],
- aside = [
+var aside = [
     {
         id: 0,
         type: "Profile",
@@ -78,37 +40,6 @@ var data = [
         iClass: ""
     }
 ];
-
-function postHTML({id, imgTitle, title, date, content, images, view, like, viewMore, html}) {
-    function getDate(){
-        let dt = new Date(date);
-        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        return `${dt.getUTCDate().toString()}/${months[dt.getMonth()].slice(0, 3)}/${dt.getUTCFullYear().toString()}`;
-    }
-    var cnt = content? `<p>${content}</p><img src="${images}" alt="promotion-${id}">` : `<div>${html.replace(/↵/g, '\n')}</div>`;
-    return `<article class="dnb-post dnb-pt10 dnb-pshadow dnb-pb10 dnb-mb20">
-        <header class="dnb-pheader dnb-pl10 dnb-pr10 ">
-          <img class="dnb-w52 dnb-h52 dnb-round-avt dnb-mr10 dnb-fl" src="${imgTitle}" >
-          <h4 class="dnb-header dnb-ml10">${title}</h4>
-        </header>
-          <hr>
-        <section class="dnb-pbody">
-          <div class="dnb-pcontent">
-            ${cnt}
-          </div>
-          <footer class="dnb-pt10 dnb-pl10 dnb-pr10 dnb-mt10">
-            <span class="dnb-pr10"><i class="fa fa-eye"></i>${view}</span>
-            <span class="dnb-pr10 dnb-pl10"><i class="fa fa-thumbs-up"></i>${like}</span>
-            <span class="dnb-pl10"><i class="fa fa-calendar-check-o"></i>${getDate(date)}</span>
-            <a class="dnb-fr dnb-pr10" href="${viewMore}" target="_blank">View more<span class="fa fa-angle-double-right dnb-pl10"></span></a>
-          </footer>
-        </section>
-      </article>`
-};
-var x = ``;
-//data.forEach(function(e){
-//    x += postHTML(e);
-//});
 
 var dnb = (function() {
     function SubMdl (parentId){ 
@@ -173,8 +104,7 @@ var dnb = (function() {
               formGoogleSend: "lockup",
               formGoogleSheetName: "responses"
             };
-          new SubMdl().service().post("https://script.google.com/macros/s/AKfycbxHxJ5kp7DRo63AfLu6fdO_wb_b0QIqjDalRSQxi4F8KQL94t0/exec",
-                                            {data: d});
+          new SubMdl().service().post("https://script.google.com/macros/s/AKfycbxHxJ5kp7DRo63AfLu6fdO_wb_b0QIqjDalRSQxi4F8KQL94t0/exec", {data: d});
       }});
     }
     
@@ -184,45 +114,46 @@ var dnb = (function() {
 })();
 
 window.onload = function(){
-    var main = new dnb.SubModule("dnbPosts").service().get("https://script.google.com/macros/s/AKfycbynTAnIGk6SnlY_JTAifuPaEgLZj--2keXhCxkIDj079NfszXY/exec", {success: function(data){
-        function postHTML({id, imgTitle, title, date, content, images, view, like, viewMore, html}) {
-          function getDate(){
-            let dt = new Date(date);
-            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                return `${dt.getUTCDate().toString()}/${months[dt.getMonth()].slice(0, 3)}/${dt.getUTCFullYear().toString()}`;
-            }
-          var cnt = ``;
-            if(html){
-                cnt = `<div>${html.replace(/↵/g, '\n')}</div>`
-            } else {
-                cnt = content ? `<p>${content}</p>` : ``;
-                cnt += images ? `<img src="${images}" alt="promotion-${id}">` : ``;
-            }
+    var main = new dnb.SubModule("dnbPosts").service().get("https://script.google.com/macros/s/AKfycbynTAnIGk6SnlY_JTAifuPaEgLZj--2keXhCxkIDj079NfszXY/exec", {
+        success: function(data){
+            function postHTML({id, imgTitle, title, date, content, images, view, like, viewMore, html}) {
+              function getDate(){
+                let dt = new Date(date);
+                var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                    return `${dt.getUTCDate().toString()}/${months[dt.getMonth()].slice(0, 3)}/${dt.getUTCFullYear().toString()}`;
+                }
+              var cnt = ``;
+                if(html){
+                    cnt = `<div>${html.replace(/↵/g, '\n')}</div>`
+                } else {
+                    cnt = content ? `<p>${content}</p>` : ``;
+                    cnt += images ? `<img src="${images}" alt="promotion-${id}">` : ``;
+                }
+
+              return `<article class="dnb-post dnb-pt10 dnb-pshadow dnb-pb10 dnb-mb20">
+                    <header class="dnb-pheader dnb-pl10 dnb-pr10 ">
+                      <img class="dnb-w52 dnb-h52 dnb-round-avt dnb-mr10 dnb-fl" src="${imgTitle}" >
+                      <h4 class="dnb-header dnb-ml10">${title}</h4>
+                    </header>
+                      <hr>
+                    <section class="dnb-pbody">
+                      <div class="dnb-pcontent">
+                        ${cnt}
+                      </div>
+                      <footer class="dnb-pt10 dnb-pl10 dnb-pr10 dnb-mt10">
+                        <span class="dnb-pr10"><i class="fa fa-eye"></i>${view}</span>
+                        <span class="dnb-pr10 dnb-pl10"><i class="fa fa-thumbs-up"></i>${like}</span>
+                        <span class="dnb-pl10"><i class="fa fa-calendar-check-o"></i>${getDate(date)}</span>
+                        <a class="dnb-fr dnb-pr10" href="${viewMore}" target="_blank">View more<span class="fa fa-angle-double-right dnb-pl10"></span></a>
+                      </footer>
+                    </section>
+                  </article>`
+            };
+
+            var x = ``;
+            data.forEach(function(e){ x += postHTML(e); });
             
-          return `<article class="dnb-post dnb-pt10 dnb-pshadow dnb-pb10 dnb-mb20">
-                <header class="dnb-pheader dnb-pl10 dnb-pr10 ">
-                  <img class="dnb-w52 dnb-h52 dnb-round-avt dnb-mr10 dnb-fl" src="${imgTitle}" >
-                  <h4 class="dnb-header dnb-ml10">${title}</h4>
-                </header>
-                  <hr>
-                <section class="dnb-pbody">
-                  <div class="dnb-pcontent">
-                    ${cnt}
-                  </div>
-                  <footer class="dnb-pt10 dnb-pl10 dnb-pr10 dnb-mt10">
-                    <span class="dnb-pr10"><i class="fa fa-eye"></i>${view}</span>
-                    <span class="dnb-pr10 dnb-pl10"><i class="fa fa-thumbs-up"></i>${like}</span>
-                    <span class="dnb-pl10"><i class="fa fa-calendar-check-o"></i>${getDate(date)}</span>
-                    <a class="dnb-fr dnb-pr10" href="${viewMore}" target="_blank">View more<span class="fa fa-angle-double-right dnb-pl10"></span></a>
-                  </footer>
-                </section>
-              </article>`
-        };
-        console.log(data);
-        var x = ``;
-        data.forEach(function(e){
-            x += postHTML(e);
-        });
-        main.render({template: x});
-    }})
+            main.render({template: x});
+        }
+    })
 }
